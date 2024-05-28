2674f1d8632e3d7f0fccedd6ce891afbddef0244
rbenites <alrrebe@gmail.com>
2024-05-28 09:55:40 -0500

    add config docker

--- backend/src/config/typeorm.ts
+++ backend/src/config/typeorm.ts
@@ -9,6 +9,7 @@ const config = {
     type: 'postgres',
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    // host: 'postgresdb',
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
