0beb1d518cf1c46ff9caf058c75dd824b0803c54
Gabriel Lazo <gabriellazo48@gmail.com>
2024-05-28 20:16:50 -0300

    logica de descuento agregada, helper promociones agregado

--- front/src/app/components/gridProducts/GridProducts.tsx
+++ front/src/app/components/gridProducts/GridProducts.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import products from "../../helpers/productos";
+import products from "../../helpers/promociones";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -19,8 +19,12 @@ const GridProducts = () => {
     );
   };
 
+  const calculateDiscountedPrice = (price: number, discount: number) => {
+    return (price - (price * discount) / 100).toFixed(2);
+  };
+
   return (
-    <div className=" mx-auto p-4 ">
+    <div className="mx-auto p-4">
       <div className="flex justify-end mb-4">
         <button onClick={handlePrevClick} disabled={startIndex === 0}>
           <Image
@@ -34,7 +38,7 @@ const GridProducts = () => {
         <button
           onClick={handleNextClick}
           disabled={startIndex >= products.length - itemsPerPage}
-          className=" ml-3"
+          className="ml-3"
         >
           <Image
             src="/Flecha-derecha.png"
@@ -45,7 +49,7 @@ const GridProducts = () => {
           />
         </button>
       </div>
-      <div className=" mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-11/12 justify-items-center">
+      <div className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-11/12 justify-items-center">
         {products
           .slice(startIndex, startIndex + itemsPerPage)
           .map((product) => (
@@ -54,7 +58,7 @@ const GridProducts = () => {
               className="flex flex-col items-center border border-gray-300 overflow-hidden rounded-lg relative"
             >
               {product.discount !== 0 && (
-                <div className="bg-red-600 rounded-xl p-1 px-2 absolute left-28 top-5 z-10">
+                <div className="bg-red-600 rounded-xl p-1 px-2 absolute left-10 top-5 z-10">
                   <p className="font-bold text-white">-{product.discount}%</p>
                 </div>
               )}
@@ -68,9 +72,25 @@ const GridProducts = () => {
               <div className="p-4 text-center">
                 <h2 className="font-bold text-lg mt-2">{product.name}</h2>
                 <p className="text-gray-600 mt-1">{product.description}</p>
-                <p className="text-2xl font-bold">${product.price}</p>
-
-                <button className=" bg-orange-500 rounded-xl p-2 px-10 mt-2 text-white font-bold border-none">
+                {product.discount !== 0 ? (
+                  <div className="text-2xl font-bold">
+                    <span className="text-gray-500 line-through mr-2">
+                      ${product.price.toFixed(2)}
+                    </span>
+                    <span>
+                      $
+                      {calculateDiscountedPrice(
+                        product.price,
+                        product.discount
+                      )}
+                    </span>
+                  </div>
+                ) : (
+                  <p className="text-2xl font-bold">
+                    ${product.price.toFixed(2)}
+                  </p>
+                )}
+                <button className="bg-orange-500 rounded-xl p-2 px-10 mt-2 text-white font-bold border-none">
                   <Link href={`/product/${product.id}`} key={product.id}>
                     Ver más
                   </Link>
