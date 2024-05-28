b6b883dbcb11a2832ef7023004ef140bb577d30f
rbRobert <rbalcazar2020.1@gmail.com>
2024-05-28 16:49:05 -0500

    actualizando

--- backend/src/guards/auth.guard.ts
+++ backend/src/guards/auth.guard.ts
@@ -1,6 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
-import { Request } from "express";
 import { Observable } from "rxjs";
 
 @Injectable()
