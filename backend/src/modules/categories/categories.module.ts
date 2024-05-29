0a8e2b54332d111caad533712da663d4cb680f53
Omarbit26 <155489881+Omarbit26@users.noreply.github.com>
2024-05-29 10:55:20 -0500

    add endpoints off categories

--- backend/src/modules/categories/categories.module.ts
+++ backend/src/modules/categories/categories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Categories } from 'src/entities/categories.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Categories])],
   providers: [CategoriesService],
   controllers: [CategoriesController]
 })
