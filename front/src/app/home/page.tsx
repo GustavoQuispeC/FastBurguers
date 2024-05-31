2fbf85b97e6738b3a89f2c61fa8a62426c486e81
Gabriel Lazo <gabriellazo48@gmail.com>
2024-05-31 15:02:35 -0300

    distribucion de carpetas modificada

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -1,7 +1,6 @@
-import Carrusel from "../components/carrusel/Carrusel";
-import GridProducts from "../components/gridProducts/GridProducts";
-import productosPreload from "../helpers/productos";
-
+import Carrusel from "../../components/carrusel/Carrusel";
+import GridProducts from "../../components/gridProducts/GridProducts";
+import productosPreload from "../../helpers/productos";
 
 const Home = () => {
   // Filtrar productos de la categoría de promociones
@@ -15,7 +14,6 @@ const Home = () => {
 
   return (
     <div className=" dark:bg-gray-800 my-10">
-     
       <Carrusel />
 
       <h1 className="font-bold mt-10 ml-5">
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,29 +1,23 @@
-"use client"
+"use client";
 
-
-import HeadlineCards from "@/app/components/headlineCards/HeadlineCards";
-import Hero from "@/app/components/hero/hero";
-import Products from "@/app/components/Products/Products";
-import Banner from "@/app/components/Banner/Banner";
+import HeadlineCards from "@/components/headlineCards/HeadlineCards";
+import Hero from "@/components/hero/hero";
+import Products from "@/components/Products/Products";
+import Banner from "@/components/Banner/Banner";
 import Head from "next/head";
 import React from "react";
-import Testimonials from "./components/Testimonials/Testimonials";
+import Testimonials from "../components/Testimonials/Testimonials";
 
-
-
-const Home= () => {
+const Home = () => {
   return (
-  
-      <div> 
-
-    <Hero/>
-  <HeadlineCards/>
-  <Products/>
-  <Banner/>
-  <Testimonials/>
-  </div>
-   
+    <div>
+      <Hero />
+      <HeadlineCards />
+      <Products />
+      <Banner />
+      <Testimonials />
+    </div>
   );
-;}
+};
 
 export default Home;
