27aaf005b86bb1004b5050cb2a96ac7cf60dbdc5
Gabriel Lazo <gabriellazo48@gmail.com>
2024-06-04 19:49:25 -0300

    codigo de paypal refinado

--- front/src/components/PayPalButton/PayPalButton.tsx
+++ front/src/components/PayPalButton/PayPalButton.tsx
@@ -2,67 +2,22 @@
 
 import { PayPalButtons } from "@paypal/react-paypal-js";
 import { useState } from "react";
+const apiURL = process.env.NEXT_PUBLIC_API_URL;
+function Message({ content }: any) {
+  return <p>{content}</p>;
+}
 
 const PayPalButton: React.FC = () => {
   const [message, setMessage] = useState("");
-  const handleApprove = async (data: any, actions: any) => {
+
+  const handlecreateOrder = async () => {
     try {
-      const response = await fetch(`/api/orders/${data.orderID}/capture`, {
+      const response = await fetch(`${apiURL}/payments/create-order`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-      });
-
-      const orderData = await response.json();
-      // Three cases to handle:
-      //   (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-      //   (2) Other non-recoverable errors -> Show a failure message
-      //   (3) Successful transaction -> Show confirmation or thank you message
-
-      const errorDetail = orderData?.details?.[0];
-
-      if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
-        // (1) Recoverable INSTRUMENT_DECLINED -> call actions.restart()
-        // recoverable state, per https://developer.paypal.com/docs/checkout/standard/customize/handle-funding-failures/
-        return actions.restart();
-      } else if (errorDetail) {
-        // (2) Other non-recoverable errors -> Show a failure message
-        throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
-      } else {
-        // (3) Successful transaction -> Show confirmation or thank you message
-        // Or go to another URL:  actions.redirect('thank_you.html');
-        const transaction = orderData.purchase_units[0].payments.captures[0];
-        setMessage(
-          `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`
-        );
-        console.log(
-          "Capture result",
-          orderData,
-          JSON.stringify(orderData, null, 2)
-        );
-      }
-    } catch (error) {
-      console.error(error);
-      setMessage(`Sorry, your transaction could not be processed...${error}`);
-    }
-  };
-
-  const createOrder = async () => {
-    try {
-      const response = await fetch("/api/orders", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          cart: [
-            {
-              id: "YOUR_PRODUCT_ID",
-              quantity: "YOUR_PRODUCT_QUANTITY",
-            },
-          ],
-        }),
+        body: JSON.stringify({ amount: 10 }),
       });
 
       const orderData = await response.json();
@@ -83,21 +38,50 @@ const PayPalButton: React.FC = () => {
     }
   };
 
+  const handleApprove = async (data: any, actions: any) => {
+    try {
+      const response = await fetch(`/api/orders/${data.orderID}/capture`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const orderData = await response.json();
+
+      const errorDetail = orderData?.details?.[0];
+
+      if (errorDetail?.issue === "INSTRUMENT_DECLINED") {
+        return actions.restart();
+      } else if (errorDetail) {
+        throw new Error(`${errorDetail.description} (${orderData.debug_id})`);
+      } else {
+        // (3) Successful transaction -> Show confirmation or thank you message
+        // Or go to another URL:  actions.redirect('thank_you.html');
+        const transaction = orderData.purchase_units[0].payments.captures[0];
+        setMessage(
+          `Transaction ${transaction.status}: ${transaction.id}. See console for all available details`
+        );
+        console.log(
+          "Capture result",
+          orderData,
+          JSON.stringify(orderData, null, 2)
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      setMessage(`Sorry, your transaction could not be processed...${error}`);
+    }
+  };
+
   return (
-    <PayPalButtons
-      createOrder={(data, actions) => {
-        return actions.order.create({
-          purchase_units: [
-            {
-              amount: {
-                value: "10.00",
-              },
-            },
-          ],
-        });
-      }}
-      onApprove={handleApprove}
-    />
+    <>
+      <PayPalButtons
+        createOrder={handlecreateOrder}
+        onApprove={handleApprove}
+      />
+      <Message content={message} />
+    </>
   );
 };
 
