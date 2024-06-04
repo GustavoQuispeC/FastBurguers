b9b872a6cc2d579de3a94eabd985f8c38622cdfe
Omarbit26 <155489881+Omarbit26@users.noreply.github.com>
2024-06-04 12:37:18 -0500

    fix module statushistory bug

--- backend/src/modules/status-histories/status-histories.module.ts
+++ backend/src/modules/status-histories/status-histories.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { StatusHistoriesController } from './status-histories.controller';
 import { StatusHistoriesService } from './status-histories.service';
+import { Orders } from 'src/entities/orders.entity';
+import { OrderDetails } from 'src/entities/orderdetails.entity';
+import { StatusHistory } from 'src/entities/statushistory.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Orders, OrderDetails, StatusHistory])],
   controllers: [StatusHistoriesController],
   providers: [StatusHistoriesService]
 })
