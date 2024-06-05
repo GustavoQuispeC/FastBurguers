f171878649314df1621260becc3275b86b3ab2d5
Gabriel Lazo <gabriellazo48@gmail.com>
2024-06-05 19:05:23 -0300

    vuelto al estado original

--- backend/src/paypal/payments.controller.ts
+++ backend/src/paypal/payments.controller.ts
@@ -1,25 +1,23 @@
-import { Body, Controller, Get, Param, Post, Request } from "@nestjs/common";
-import { PaymentsService } from "./payments.service";
-import { CreateOrderDto } from "./dto/payments.dto";
+import { Body, Controller, Get, Param, Post, Request } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { CreateOrderDto } from './dto/payments.dto';
 
 @Controller('payments')
 export class PaymentsController {
-    constructor(private readonly paymentsService: PaymentsService){}
+  constructor(private readonly paymentsService: PaymentsService) {}
 
-    @Post('create-order')
-    async createOrder(
-        @Body() createOrderDto: CreateOrderDto) {
-            return await this.paymentsService.createOrder(createOrderDto.amount)
-        }
+  @Post('create-order')
+  async createOrder(@Body() createOrderDto: CreateOrderDto) {
+    return await this.paymentsService.createOrder(createOrderDto.amount);
+  }
 
+  @Post('capture-order/:orderId')
+  async captureOrder(@Param('orderId') orderId: string) {
+    return await this.paymentsService.captureOrder(orderId);
+  }
 
-    @Post('capture-order/:orderId')
-        async captureOrder(@Param('orderId') orderId: string) {
-            return await this.paymentsService.captureOrder(orderId);
-        }
-        
-    @Post('cancel-order/:orderId')
-        async cancelOrder(@Param('orderId') orderId: string) {
-            return await this.paymentsService.cancelOrder(orderId);
-        }
+  @Post('cancel-order/:orderId')
+  async cancelOrder(@Param('orderId') orderId: string) {
+    return await this.paymentsService.cancelOrder(orderId);
+  }
 }
--- backend/src/paypal/payments.service.ts
+++ backend/src/paypal/payments.service.ts
@@ -1,63 +1,63 @@
-import { Injectable } from "@nestjs/common";
-import * as paypal from "@paypal/checkout-server-sdk";
+import { Injectable } from '@nestjs/common';
+import * as paypal from '@paypal/checkout-server-sdk';
 
 @Injectable()
 export class PaymentsService {
-        private readonly clientId: string = process.env.YOUR_PAYPAL_CLIENT_ID;
-        private readonly clientSecret: string = process.env.YOUR_PAYPAL_CLIENT_SECRET;
-        private readonly environment: paypal.core.Environment = new paypal.core.SandboxEnvironment(this.clientId, this.clientSecret);
-        private readonly client: paypal.PayPalHttpClient = new paypal.core.PayPalHttpClient(this.environment);
+  private readonly clientId: string = process.env.YOUR_PAYPAL_CLIENT_ID;
+  private readonly clientSecret: string = process.env.YOUR_PAYPAL_CLIENT_SECRET;
+  private readonly environment: paypal.core.Environment =
+    new paypal.core.SandboxEnvironment(this.clientId, this.clientSecret);
+  private readonly client: paypal.PayPalHttpClient =
+    new paypal.core.PayPalHttpClient(this.environment);
 
-    
+  async createOrder(amount: number): Promise<string> {
+    const request = new paypal.orders.OrdersCreateRequest();
+    request.prefer('return=representation');
+    request.requestBody({
+      intent: 'CAPTURE',
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: amount.toString(),
+          },
+        },
+      ],
+      // application_context:{
+      //     brand_name:"fastburguers.com",
+      //     user_action:"PAY_NOW",
+      //     return_url: "http://localhost:3001/capture-order",
+      //     cancel_url: "http://localhost:3001/cancel-order"
 
-    async createOrder(amount: number): Promise<string> {
-        const request = new paypal.orders.OrdersCreateRequest();
-        request.prefer("return=representation");
-        request.requestBody({
-            intent: "CAPTURE",
-            purchase_units: [{
-                amount: {
-                    currency_code: "USD",
-                    value: amount.toString(),
-                },
-            }],
-            // application_context:{
-            //     brand_name:"fastburguers.com",
-            //     user_action:"PAY_NOW",
-            //     return_url: "http://localhost:3001/capture-order",
-            //     cancel_url: "http://localhost:3001/cancel-order"
+      // }
+    });
 
-            // }
-        });
-    
-        const response = await this.client.execute(request);
-        console.log(response.result);
-        
-        return response.result.id;
+    const response = await this.client.execute(request);
+    console.log(response.result);
+
+    return response.result.id;
+  }
+
+  async captureOrder(orderId: string): Promise<any> {
+    try {
+      const request = new paypal.orders.OrdersCaptureRequest(orderId);
+      request.requestBody({});
+      const response = await this.client.execute(request);
+      return response.result;
+    } catch (error) {
+      console.error('Error capturing order:', error);
+      throw error;
     }
+  }
 
-    async captureOrder(orderId: string): Promise<any> {
-        try {
-            const request = new paypal.orders.OrdersCaptureRequest(orderId);
-            request.requestBody({});
-            const response = await this.client.execute(request);
-            return response.result;
-        } catch (error) {
-            console.error('Error capturing order:', error);
-            throw error;
-        }
+  async cancelOrder(orderId: string): Promise<any> {
+    try {
+      const request = new paypal.orders.OrdersCancelRequest(orderId);
+      const response = await this.client.execute(request);
+      return response.result;
+    } catch (error) {
+      console.error('Error cancelling order:', error);
+      throw error;
     }
-
-    async cancelOrder(orderId: string): Promise<any> {
-        try {
-            const request = new paypal.orders.OrdersCancelRequest(orderId);
-            const response = await this.client.execute(request);
-            return response.result;
-        } catch (error) {
-            console.error('Error cancelling order:', error);
-            throw error;
-        }
-    }
-    
-
+  }
 }
