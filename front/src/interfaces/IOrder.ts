684355f0dd8970ae763e5ba56869067a74ca753a
Gabriel Lazo <gabriellazo48@gmail.com>
2024-06-09 15:44:17 -0300

    cambios

--- front/src/interfaces/IOrder.ts
+++ front/src/interfaces/IOrder.ts
@@ -2,12 +2,12 @@ import { IOrderDetails, IPedido, IStatusHistory } from "./IPedido";
 import { IProduct } from "./IProduct";
 
 export interface IOrderUser {
-    name: string;
-    id: string;
-    email: string;
-    address: string;
-    phone: number;
-    city: string;
-    country: string;
-    orders:IPedido[];
-  }
+  name: string;
+  id: string;
+  email: string;
+  address: string;
+  phone: number;
+  city: string;
+  country: string;
+  orders: IPedido[];
+}
