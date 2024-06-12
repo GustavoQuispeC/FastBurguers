d2c622af906303823b76bdf8f1e3654e2af94927
Gabriel Lazo <gabriellazo48@gmail.com>
2024-06-12 17:04:25 -0300

    paypal modificado

--- backend/src/paypal/payments.controller.ts
+++ backend/src/paypal/payments.controller.ts
@@ -1,33 +1,38 @@
-import {Body, Controller, Param, Post } from '@nestjs/common';
-import { PaymentsService} from './payments.service';
-import { CreateOrderDto} from './dto/payments.dto';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { CreateOrderDto } from './dto/payments.dto';
 import { MailerService } from 'src/modules/mailer/mailer.service';
 import { MailerDto } from 'src/modules/mailer/mailer.dto';
 
 @Controller('payments')
 export class PaymentsController {
-    constructor(
-        private readonly paymentsService: PaymentsService,
-        private readonly mailerService: MailerService
-    ) {}
+  constructor(
+    private readonly paymentsService: PaymentsService,
+    private readonly mailerService: MailerService,
+  ) {}
 
-    @Post('create-order')
-        async createOrder(@Body() createOrderDto: CreateOrderDto) {
-        return await this.paymentsService.createOrder(createOrderDto.amount);
-    }
+  @Post('create-order')
+  async createOrder(@Body() createOrderDto: CreateOrderDto) {
+    return await this.paymentsService.createOrder(createOrderDto.amount);
+  }
 
-    @Post('capture-order/:orderId')
-        async captureOrder(@Param('orderId') orderId: string,
-        @Body() mailerDto: MailerDto) {
-            const orderDetails = {
-                items: mailerDto.items, 
-                totalAmount: mailerDto.totalAmount,
-                orderId: mailerDto.orderId,
-                customerName: mailerDto.customerName,
-            };
-        await this.paymentsService.captureOrder(orderId);
-        await this.mailerService.sendMail(mailerDto.email, 'Confirmación de Compra', orderDetails)
-        return { message: 'Orden creada y correo enviado.' };
-    }
-
+  @Post('capture-order/:orderId')
+  async captureOrder(
+    @Param('orderId') orderId: string,
+    @Body() mailerDto: MailerDto,
+  ) {
+    const orderDetails = {
+      items: mailerDto.items,
+      totalAmount: mailerDto.totalAmount,
+      orderId: mailerDto.orderId,
+      customerName: mailerDto.customerName,
+    };
+    await this.paymentsService.captureOrder(orderId);
+    await this.mailerService.sendMail(
+      mailerDto.email,
+      'Confirmación de Compra',
+      orderDetails,
+    );
+    return { message: 'Orden creada y correo enviado.' };
+  }
 }
