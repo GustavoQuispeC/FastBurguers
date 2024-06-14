9eb9e0f08dc1612fd24e1144cc310c298090f016
rbRobert <rbalcazar2020.1@gmail.com>
2024-06-14 11:02:30 -0500

    declarando campos en entidad storage

--- backend/src/entities/storage.entity.ts
+++ backend/src/entities/storage.entity.ts
@@ -1,11 +1,19 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({
     name: 'storage'
 })
 export class Storage {
     @PrimaryGeneratedColumn('uuid')
-    id: string;
+    id: string
 
+    @Column()
+    id_user: string
+
+    @Column()
+    id_product: string
+
+    @Column()
+    quantity: number
 
 }
