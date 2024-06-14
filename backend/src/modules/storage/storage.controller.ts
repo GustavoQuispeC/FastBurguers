6cd6004087de29b9f79f606a18b613744749da1a
Omarbit26 <155489881+Omarbit26@users.noreply.github.com>
2024-06-14 14:01:53 -0500

    add storage database and endpoints

--- backend/src/modules/storage/storage.controller.ts
+++ backend/src/modules/storage/storage.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, UseGuards } from '@nestjs/common';
+import { CreateOrderStorageDto } from './storage.dto';
+import { StorageService } from './storage.service';
+import { AuthGuards } from 'src/guards/auth.guard';
 
 @Controller('storage')
 export class StorageController {
 
-
-    @Get()
-    getStorageOrders(){}
+    constructor(
+        private readonly  storageService:StorageService,
+    ){}
+    @Get(':id')
+    @UseGuards(AuthGuards)
+    getStorageByID(@Param('id', ParseUUIDPipe) id:string){
+       return this.storageService.getByID(id)
+    }
 
     @Post()
-    createOrder(){}
+    @UseGuards(AuthGuards)
+    createOrder(@Body() orderInfo:CreateOrderStorageDto){
+        const {userId,products} = orderInfo;
+        return this.storageService.createOrder(userId,products)
 
-    @Delete()
-    delelteOrder(){}
+    }   
+
+    @Delete(':id')
+    @UseGuards(AuthGuards)
+    delelteOrder(@Param('id', ParseUUIDPipe) id:string){
+        return this.storageService.delete(id)
+    }
 }
