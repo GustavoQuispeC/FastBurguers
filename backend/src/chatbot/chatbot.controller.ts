bf0194dbd1aecd7feaddcce28d933b614bb4e2ae
rbenites <alrrebe@gmail.com>
2024-06-17 16:02:41 -0500

    fix flows chatbot

--- backend/src/chatbot/chatbot.controller.ts
+++ backend/src/chatbot/chatbot.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, InternalServerErrorException, Post } from '@nestjs/common';
 import { ChatbotService } from './chatbot.service';
 
 @Controller('chatbot')
@@ -6,9 +6,18 @@ export class ChatbotController {
     constructor(private readonly chatbotService: ChatbotService) {}
 
     @Post('message')
-    handleMessage(@Body() body: { userId: string; message: string }) {
+    async handleMessage(@Body() body: { userId: string; message: string }) {
         const { userId, message } = body;
-        const response = this.chatbotService.getNextStep(userId, message);
-        return { response };
+
+        if (!userId || !message) throw new BadRequestException('userId y message son requeridos');
+
+        try {
+            const response = this.chatbotService.getNextStep(userId, message);
+            return { response }; 
+        } catch (error) {
+            console.error('Error procesando el mensaje:', error);
+            throw new InternalServerErrorException('Ha ocurrido un error al procesar tu mensaje. Por favor, intenta nuevamente.');
+        }
+        
     }
 }
--- backend/src/chatbot/chatbot.service.ts
+++ backend/src/chatbot/chatbot.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { log } from 'console';
 
 @Injectable()
 export class ChatbotService {
@@ -22,86 +23,138 @@ export class ChatbotService {
     }
     const session = this.userSession.get(userId);
     let response: string;
-    switch (session.step) {
-      case 0:
-        // Paso 0: Solicitar nombre
-        response = this.steps[0];
-        session.step++;
-        break;
+
+    try{
+      switch (session.step) {
+        case 0:
+          // Paso 0: Solicitar nombre
+          response = this.steps[0];
+          session.step++;
+          break;
+        case 1:
+          // Paso 1: Saludo y pregunta sobre recomendaciones
+          session.name = message.trim();
+          response = this.steps[1].replace('{name}', session.name);
+          session.step++;
+          break;
+        case 2:
+          // Paso 2: Mostrar opciones
+          response = this.steps[2];
+          // response = this.handleOptionSelection(message, session);
+          session.step++;
+          break;
+        
+        case 3:
+          // Paso 2: redirigir a opciones
+          response = this.handleOptionSelection(message, session);
+          // session.step++;
+          break;
+        case 4:
+          // Paso 3: Mostrar hamburguesas del día
+          response = this.handleBurgerOptions(message, session);          
+          break;
+        case 5:
+          // Paso 4: Mostrar información adicional
+          response = this.handleAdditionalInfo(message, session);
+          break;
+        case 6:
+          // Paso 5: Mostrar locales
+          response = this.handleLocationInfo(message, session);
+          break;
+        case 7:
+          // Paso 6: Mostrar números de contacto
+          response = this.handleContactInfo(message, session);
+          break;
+        case 8:
+          // Paso 7: Despedida
+          response = this.steps[7];
+          // Reiniciar la conversación eliminando la sesión del usuario
+          this.userSession.delete(userId);
+          break;
+        default:
+          // Por defecto, si se alcanza este caso, se termina la conversación
+          response = 'Gracias por tu tiempo. ¡Hasta luego!';
+          this.userSession.delete(userId);
+      }
+    } catch (error) {
+      response = 'Ha ocurrido un error. Por favor, intenta nuevamente.';
+      console.error(`Error handling step ${session.step} for user ${userId}:`, error);
+    }
+    
+    return response;
+  }
+
+  private handleOptionSelection(message: string, session: { step: number; name?: string }): string {
+    const option = parseInt(message, 10);
+
+    if (isNaN(option) || option < 1 || option > 3) {
+      return 'Opción inválida. Por favor, selecciona una opción correcta.';
+    }
+
+    switch (option) {
       case 1:
-        // Paso 1: Saludo y pregunta sobre recomendaciones
-        session.name = message;
-        response = this.steps[1].replace('{name}', session.name);
-        session.step++;
+        session.step = 3;
         break;
       case 2:
-        // Paso 2: Mostrar opciones
-        response = this.steps[2];
-        session.step++;
+        session.step = 5;
         break;
       case 3:
-        // Paso 3: Mostrar hamburguesas del día
-        if (message === '1') {
-          response = this.steps[3];
-          session.step++;
-        } else if (message === '2') {
-          response = this.steps[2];
-          session.step = 2; // Volver al paso 2
-        } else {
-          response =
-            'Opción inválida. Por favor, selecciona una opción válida.';
-        }
+        session.step = 6;
         break;
-      case 4:
-        // Paso 4: Mostrar información adicional
-        if (message === '1') {
-          response = this.steps[4];
-          session.step++;
-        } else if (message === '2') {
-          response = this.steps[2];
-          session.step = 2; // Volver al paso 2
-        } else {
-          response =
-            'Opción inválida. Por favor, selecciona una opción válida.';
-        }
-        break;
-      case 5:
-        // Paso 5: Mostrar locales
-        if (message === '1') {
-          response = this.steps[5];
-          session.step++;
-        } else if (message === '2') {
-          response = this.steps[2];
-          session.step = 2; // Volver al paso 2
-        } else {
-          response =
-            'Opción inválida. Por favor, selecciona una opción válida.';
-        }
-        break;
-      case 6:
-        // Paso 6: Mostrar números de contacto
-        if (message === '1') {
-          response = this.steps[6];
-          session.step++;
-        } else if (message === '2') {
-          response = this.steps[2];
-          session.step = 2; // Volver al paso 2
-        } else {
-          response =
-            'Opción inválida. Por favor, selecciona una opción válida.';
-        }
-        break;
-      case 7:
-        // Paso 7: Despedida
-        response = this.steps[7];
-        // Reiniciar la conversación eliminando la sesión del usuario
-        this.userSession.delete(userId);
-        break;
-      default:
-        // Por defecto, si se alcanza este caso, se termina la conversación
-        response = 'Gracias por tu tiempo. ¡Hasta luego!';
-        this.userSession.delete(userId);
     }
-    return response;
+
+    return this.steps[session.step];
+  }
+
+  private handleBurgerOptions(message: string, session: { step: number; name?: string }): string {
+    switch (message) {
+      case '1':
+        session.step = 4;
+        return this.steps[3];
+      case '2':
+        session.step = 2;
+        return this.steps[2];
+      default:
+        return 'Opción inválida. Por favor, selecciona una opción correcta.';
+    }
+  }
+
+  private handleAdditionalInfo(message: string, session: { step: number; name?: string }): string {
+    switch (message) {
+      case '1':
+        session.step = 2;
+        return this.steps[2];
+      case '2':
+        session.step = 7;
+        return this.steps[7];
+      default:
+        return 'Opción inválida. Por favor, selecciona una opción correcta.';
+    }
+  }
+
+  private handleLocationInfo(message: string, session: { step: number; name?: string }): string {
+    switch (message) {
+      case '1':
+        session.step = 2;
+        return this.steps[2];
+      case '2':
+        session.step = 7;
+        return this.steps[7];
+      default:
+        return 'Opción inválida. Por favor, selecciona una opción correcta.';
+    }
+  }
+
+  private handleContactInfo(message: string, session: { step: number; name?: string }): string {
+    switch (message) {
+      case '1':
+        session.step = 2;
+        return this.steps[2];
+      case '2':
+        session.step = 7;
+        return this.steps[7];
+      default:
+        return 'Opción inválida. Por favor, selecciona una opción correcta.';
+    }
   }
 }
