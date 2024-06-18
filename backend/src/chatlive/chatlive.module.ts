df223c8ac7c27f0abdce5f61f33888421ac7d1cc
Omarbit26 <155489881+Omarbit26@users.noreply.github.com>
2024-06-17 23:45:18 -0500

    add canales websockets

--- backend/src/chatlive/chatlive.module.ts
+++ backend/src/chatlive/chatlive.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ChatsocketGateway } from './chatlive.gateway';
+import { AdminGateway } from './admin.gateway';
 
 @Module({
-    imports:[ChatsocketGateway]
+    imports:[ChatsocketGateway,AdminGateway]
 })
 export class ChatliveModule {}
