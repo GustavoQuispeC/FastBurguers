fe15775699aed9e8d4106c34aee791b5e2384da8
Gabriel Lazo <gabriellazo48@gmail.com>
2024-06-18 04:40:25 -0300

    ninguna modificacion, solo saltos de linea

--- backend/src/chatlive/admin.gateway.ts
+++ backend/src/chatlive/admin.gateway.ts
@@ -1,14 +1,17 @@
-import { WebSocketGateway, WebSocketServer, OnGatewayInit } from '@nestjs/websockets';
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({ namespace: '/admin' })
 export class AdminGateway implements OnGatewayInit {
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('AdminGateway');
 
-    @WebSocketServer() server: Server;
-    private logger: Logger = new Logger('AdminGateway');
-
-    afterInit(server: Server) {
+  afterInit(server: Server) {
     this.logger.log('Admin namespace initialized');
-    }
+  }
 }
--- backend/src/chatlive/chatlive.gateway.ts
+++ backend/src/chatlive/chatlive.gateway.ts
@@ -1,42 +1,50 @@
-import { Logger } from "@nestjs/common";
-import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server,Socket} from "socket.io";
+import { Logger } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(3002,{
-    cors:{
-        origin:'*'
-    }
+@WebSocketGateway(3002, {
+  cors: {
+    origin: '*',
+  },
 })
-export class ChatsocketGateway implements OnGatewayConnection, OnGatewayDisconnect{
+export class ChatsocketGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
 
-    @WebSocketServer()
-    server:Server;
-    
-    private logger: Logger = new Logger('AppGateway');
+  private logger: Logger = new Logger('AppGateway');
 
-    afterInit(server: Server) {
-        console.log('Socket service funcionando en puerto 3002')
-    }
+  afterInit(server: Server) {
+    console.log('Socket service funcionando en puerto 3002');
+  }
 
-    handleConnection(client: Socket) {
-        this.logger.log(`Client connected: ${client.id}`)
-    }
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
 
-    handleDisconnect(client: Socket) {
-        this.logger.log(`Client disconnected: ${client.id}`);
-    }
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
 
-    @SubscribeMessage('join')
-    handleJoinRoom(client: Socket, room: string): void {
+  @SubscribeMessage('join')
+  handleJoinRoom(client: Socket, room: string): void {
     client.join(room);
     this.logger.log(`Client ${client.id} joined room ${room}`);
     this.server.of('/admin').emit('new_room', room);
-    }
+  }
 
-    @SubscribeMessage('message')
-    handleMessage(client: Socket, data: { room: string, message: string }): void {
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, data: { room: string; message: string }): void {
     const { room, message } = data;
     this.server.to(room).emit('message', message);
-}
-    
+  }
 }
